c3ac874b2d47963ac892e17eb1f2e155ba00dca2
KimHakSeong1203 <81869289+KimHakSeong1203@users.noreply.github.com>
2021-04-22 09:25:54 +0900

    Rename C_Practice(1_2).cpp to C_Practice(1_2).c

---
+++ C_Practice(1_2).c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main() {
+	printf("\nÁÙ¹Ù²Þ\n");
+	printf("\tÅÇ");
+	printf("\\Ãâ·Â"); 
+	
+	return 0;
+}
--- C_Practice(1_2).cpp
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main() {
-	printf("\nÁÙ¹Ù²Ş\n");
-	printf("\tÅÇ");
-	printf("\\Ãâ·Â"); 
-	
-	return 0;
-}
